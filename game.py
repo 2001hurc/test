72f977fe24691a822e28c704b81b41d63f30c8f1
2001hurc <hurc2001@gmail.com>
2018-09-02 09:33:42 +0800

    修改了一些数据

--- game.py
+++ game.py
@@ -1,17 +1,14 @@
-import random
-secret = random.randint(1,10)
 print('------------------我爱鱼C工作室------------------')
-temp = input("不妨猜一下小甲鱼现在心里想的是哪个数字：")
-guess = int(temp)
-while guess != secret:
-    temp = input("哎呀，猜错了，请重新输入吧：")
+count=0
+while count < 3:
+    temp = input("不妨猜一下小甲鱼现在心里想的是哪个数字：")
     guess = int(temp)
-    if guess == secret:
-        print("我草，你是小甲鱼心里的蛔虫吗？！")
-        print("哼，猜中了也没有奖励！")
+    if guess == 8:
+     print("我草，你是小甲鱼心里的蛔虫吗？！")
+     print("哼，猜中了也没有奖励！")
+     break
     else:
-        if guess > secret:
-            print("哥，大了大了~~~")
-        else:
-            print("嘿，小了，小了~~~")
+      print("猜错拉，小甲鱼现在心里想的是8！")
+    count +=1
+
 print("游戏结束，不玩啦^_^")
